a5a513aaaddee3c2a1262478ec9ea9ef4c559076
Vickie Hughes <vickie_hughes@hotmail.com>
2021-06-14 17:11:14 -0500

    upload

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,19 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    tbody.html("");
+    
+    data.forEach((dataRow) => {
+        var row = tbody.append("tr");
+        
+        Object.values(dataRow).forEach((val) => {var cell = row.append("td");
+        cell.text(val);
+    });
+});
+}
+
+buildTable(tableData);
